619f7f5d285e70881f3c990d5a32fa60db4ad3af
DanyBoss <danielcarmo93@gmail.com>
2018-07-01 13:30:54 +0100

    Cleanup

--- GrandHeroFarmer/Helpers/Android.cs
+++ GrandHeroFarmer/Helpers/Android.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Feh_Farmer.Helpers
 {
@@ -24,12 +25,15 @@ namespace Feh_Farmer.Helpers
             monitor.Start();
         }
 
+        /// <summary>
+        /// Tries to connect to an android device by using the ADB interface.
+        /// </summary>
         private void ConnectToAndroid()
         {
             bool foundDevice = false;
             while (!foundDevice)
             {
-                ConsoleLogger.Write("Trying to get Device from adb...", Type.Default);
+                ConsoleLogger.Write("Trying to get device from adb...", Type.Default);
                 if(AdbClient.Instance.GetDevices().Count != 0)
                 {
                     _device = AdbClient.Instance.GetDevices().First();
@@ -37,7 +41,8 @@ namespace Feh_Farmer.Helpers
                     ConsoleLogger.WriteLine(_device.Name + " connected!", Type.Info, false);
                     return;
                 }
-                ConsoleLogger.WriteLine("No Device was found! Is it connected?", Type.Error);
+
+                ConsoleLogger.WriteLine("No Device was found! Is the phone connected?", Type.Error);
                 ConsoleLogger.Write("Check the connection and press", Type.Default);
                 ConsoleLogger.Write(" Enter ", Type.Info, false);
                 ConsoleLogger.WriteLine("to try again.", Type.Default, false);
@@ -48,12 +53,11 @@ namespace Feh_Farmer.Helpers
         /// <summary>
         /// Sends a tap event to the connected device with the given coordenates
         /// </summary>
-        /// <param name="pX"></param>
-        /// <param name="pY"></param>
+        /// <param name="pX">X Coordinate</param>
+        /// <param name="pY">Y Coordinate</param>
         public void Tap(Tuple<int, int> coordenates)
         {
             string command = "input tap " + coordenates.Item1 + " " + coordenates.Item2;
-
 
             IShellOutputReceiver receiver = null;
 
@@ -64,6 +68,7 @@ namespace Feh_Farmer.Helpers
         void OnDeviceDisconnected(object sender, DeviceDataEventArgs e)
         {
             ConsoleLogger.WriteLine("The device " + _device.Name +" has disconnected to this PC", Type.Error);
+            ConsoleLogger.WriteLine("Please restart the application and go back to the Mission Select Menu.", Type.Default);
             ConnectToAndroid();
         }
     }
@@ -79,8 +84,21 @@ namespace Feh_Farmer.Helpers
         public ClickArea(int pTopLeftX, int pTopLeftY, int pBottomRightX, int pBottomRightY)
         {
             topLeft = new Tuple<int, int>(pTopLeftX, pTopLeftY);
-            bottomRight = new Tuple<int, int>(pBottomRightX, pBottomRightY); ;
+            bottomRight = new Tuple<int, int>(pBottomRightX, pBottomRightY);
         }
+
+        /// <summary>
+        /// Creates a click area from a XElement in a XML File.
+        /// </summary>
+        /// <param name="element"></param>
+        public ClickArea(XElement element) 
+            : this
+            (
+                (int) element.Descendants("TopLeftX").FirstOrDefault(),
+                (int) element.Descendants("TopLeftY").FirstOrDefault(),
+                (int) element.Descendants("BottomRightX").FirstOrDefault(),
+                (int) element.Descendants("BottomRightY").FirstOrDefault()
+            ) { }
 
         public Tuple<int, int> GenerateRandomCoords()
         {
---
+++ GrandHeroFarmer/Program.cs
@@ -0,0 +1,83 @@
+﻿using Feh_Farmer.Helpers;
+using SharpAdbClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Feh_Farmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleLogger.WriteLine("Welcome to Feh Farmer", Helpers.Type.Info, true, ConsoleColor.DarkYellow);
+            Android phone = new Android();
+
+            //Initializing Service Configurations
+            ConsoleLogger.Write("Loading service configurations from xml... ", Helpers.Type.Default);
+            XDocument doc = XDocument.Load("Configurations/Default.xml");
+
+            ClickArea startGBHButton = new ClickArea(doc.Descendants("StartGHBButton").FirstOrDefault());
+            ClickArea fightButton = new ClickArea(doc.Descendants("FightButton").FirstOrDefault());
+            ClickArea skipDialogButton = new ClickArea(doc.Descendants("SkipDialogButton").FirstOrDefault());
+            ClickArea autoBattleButton = new ClickArea(doc.Descendants("AutoBattleButton").FirstOrDefault());
+            ClickArea acceptAutoBattleButton = new ClickArea(doc.Descendants("AcceptAutoBattleButton").FirstOrDefault());
+
+            int communicateServerTimer = ((int)(doc.Descendants("CommunicateServerTimer").FirstOrDefault()) * 1000);
+            int stageLoadTimer = ((int)(doc.Descendants("StageLoadTimer").FirstOrDefault()) * 1000);
+            int initialDialogTimer = ((int)(doc.Descendants("InitialDialogTimer").FirstOrDefault()) * 1000);
+            int stageTimer = ((int)(doc.Descendants("StageTimer").FirstOrDefault()) * 1000);
+
+            ConsoleLogger.WriteLine("OK", Helpers.Type.Info, false);
+
+            ConsoleLogger.WriteLine("Setup is done, press enter to start earning feathers.", Helpers.Type.Info);
+            ConsoleLogger.WriteLine("Press 'Ctrl + C' to exit the application.", Helpers.Type.Info);
+            Console.ReadLine();
+
+            int iterations = 0;
+
+            Thread thread = new Thread(() => {
+                while (true)
+                {
+                    Console.WriteLine();
+                    ConsoleLogger.Write("Starting iteration ", Helpers.Type.Default);
+                    ConsoleLogger.WriteLine(iterations.ToString(), Helpers.Type.Info, false);
+                    phone.Tap(startGBHButton.GenerateRandomCoords());
+
+                    phone.Tap(fightButton.GenerateRandomCoords());
+
+                    Thread.Sleep(communicateServerTimer);
+                    phone.Tap(fightButton.GenerateRandomCoords());
+
+                    Thread.Sleep(stageLoadTimer);
+                    ConsoleLogger.WriteLine("Skipping initial dialog", Helpers.Type.Default);
+                    phone.Tap(skipDialogButton.GenerateRandomCoords());
+                    Thread.Sleep(initialDialogTimer);
+
+                    ConsoleLogger.WriteLine("Initializing Auto-Battle", Helpers.Type.Default);
+                    phone.Tap(autoBattleButton.GenerateRandomCoords());
+
+                    phone.Tap(acceptAutoBattleButton.GenerateRandomCoords());
+
+                    ConsoleLogger.WriteLine("Waiting for battle to end", Helpers.Type.Default);
+                    Thread.Sleep(stageTimer);
+
+                    phone.Tap(fightButton.GenerateRandomCoords());
+                    Thread.Sleep(communicateServerTimer);
+
+                    ConsoleLogger.Write("Finished iteration ", Helpers.Type.Default);
+                    ConsoleLogger.WriteLine(iterations.ToString(), Helpers.Type.Info, false);
+
+                    iterations++;
+                }
+
+            });
+            thread.Start();
+        }
+    }
+}
