78d4439810790b3cad93fbcf7f4f5024a6866717
Daniel Carmo <danielcarmo93@gmail.com>
2018-07-02 21:55:52 +0100

    v0.02 release

--- GrandHeroFarmer/Properties/AssemblyInfo.cs
+++ GrandHeroFarmer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GrandHeroFarmer")]
-[assembly: AssemblyDescription("For some farming needs.")]
+[assembly: AssemblyDescription("For the usual farmer.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GrandHeroFarmer")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
